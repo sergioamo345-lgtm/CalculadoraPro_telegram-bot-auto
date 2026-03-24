b936e9babdaca4c4b901fa3da3af51f38ab4f614
sergioamo345-lgtm <sergioamo345@gmail.com>
2026-03-24 17:32:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-// pega o token das variáveis do Railway
 const token = process.env.TELEGRAM_TOKEN;
 
-// cria o bot
 const bot = new TelegramBot(token, { polling: true });
 
-// comando /start
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, 'Bot funcionando 👊');
-});
-
-// comando de teste
-bot.onText(/\/teste/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Teste OK ✅');
 });
 
 console.log('Bot rodando...');
